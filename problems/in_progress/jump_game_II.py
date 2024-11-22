d807d5fbf5422eac88f0da68dfba58d4a7821a1c
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-11-21 23:15:51 -0600

    jump game II

--- problems/in_progress/jump_game_II.py
+++ problems/in_progress/jump_game_II.py
@@ -1,9 +1,30 @@
+# Inefficient DP solution
 def jump(nums):
     """
     :type nums: List[int]
     :rtype: int
     """
     
-    n = len(nums)
+    final_index = len(nums) - 1
 
-    
+    outcomes = [float("inf") for i in range(final_index+1)]
+
+    outcomes[0] = 0
+
+    for index, value in enumerate(nums):
+        jumps = outcomes[index]+1
+
+        if (index + value) < final_index:
+            check_range = value
+        else:
+            check_range = final_index - index
+
+        for i in range(1, check_range+1):
+            if outcomes[index+i] > jumps:
+                outcomes[index+i] = jumps
+
+    return outcomes[final_index]
+
+print(jump([1,3,4,15,0,0,1,2,1]))
+print(jump([2,3,1,1,4]))
+print(jump([2,3,0,1,4]))
