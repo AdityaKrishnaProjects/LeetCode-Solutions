2c1e35627678f4e4c3644821eb93403ca92715b9
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-11-28 04:21:15 -0600

    er the

---
+++ problems/solved/easy/valid_parenthesis.py
@@ -0,0 +1,42 @@
+# more domain general solution (accepts random chars between parenthesese)
+def isValid(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+
+    stack = []
+    par_dict = {"[":"]", "{":"}", "(":")"}
+    left_par = ["[", "{", "("]
+    right_par = ["]", "}", ")"]
+
+    for char in s:
+        if char in left_par:
+            stack.append(char)
+            continue
+
+        N = len(stack)
+
+        if N == 0:
+            if char in right_par:
+                return False
+            else:
+                continue
+
+        if char in right_par:
+            if char != par_dict[stack[N-1]]:
+                return False
+            else: 
+                stack.pop()
+
+    return not stack
+
+print(isValid("()"))
+print(isValid("()[]{}"))
+print(isValid("(]"))
+print(isValid("()"))
+print(isValid("([])"))
+print(isValid("(["))
+print(isValid("(adwqdwqe[qweqweqwe]wee)"))
+print(isValid("(adwqdwqe[qweqweqwe{wee)"))
+print(isValid("}}"))
