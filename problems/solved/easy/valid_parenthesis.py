2e531c633aec28d772f301443c1672a1dbcb261b
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-05 02:26:13 -0600

    parenthesis logic clean up

--- problems/solved/easy/valid_parenthesis.py
+++ problems/solved/easy/valid_parenthesis.py
@@ -1,10 +1,52 @@
-# more domain general solution (accepts random chars between parenthesese)
+# # more domain general solution (accepts random chars between parenthesese)
+# def isValid(s):
+#     """
+#     :type s: str
+#     :rtype: bool
+#     """
+
+#     stack = []
+#     par_dict = {"[":"]", "{":"}", "(":")"}
+#     left_par = ["[", "{", "("]
+#     right_par = ["]", "}", ")"]
+
+#     for char in s:
+#         if char in left_par:
+#             stack.append(char)
+#             continue
+
+#         N = len(stack)
+
+#         if N == 0:
+#             if char in right_par:
+#                 return False
+#             else:
+#                 continue
+
+#         if char in right_par:
+#             if char != par_dict[stack[N-1]]:
+#                 return False
+#             else: 
+#                 stack.pop()
+
+#     return not stack
+
+# print(isValid("()"))
+# print(isValid("()[]{}"))
+# print(isValid("(]"))
+# print(isValid("()"))
+# print(isValid("([])"))
+# print(isValid("(["))
+# print(isValid("(adwqdwqe[qweqweqwe]wee)"))
+# print(isValid("(adwqdwqe[qweqweqwe{wee)"))
+# print(isValid("}}"))
+
+# more domain general and cleaner than the above
 def isValid(s):
     """
     :type s: str
     :rtype: bool
     """
-
     stack = []
     par_dict = {"[":"]", "{":"}", "(":")"}
     left_par = ["[", "{", "("]
@@ -17,26 +59,12 @@ def isValid(s):
 
         N = len(stack)
 
-        if N == 0:
-            if char in right_par:
-                return False
-            else:
-                continue
-
         if char in right_par:
+            if N == 0:
+                return False
             if char != par_dict[stack[N-1]]:
                 return False
             else: 
                 stack.pop()
 
     return not stack
-
-print(isValid("()"))
-print(isValid("()[]{}"))
-print(isValid("(]"))
-print(isValid("()"))
-print(isValid("([])"))
-print(isValid("(["))
-print(isValid("(adwqdwqe[qweqweqwe]wee)"))
-print(isValid("(adwqdwqe[qweqweqwe{wee)"))
-print(isValid("}}"))
