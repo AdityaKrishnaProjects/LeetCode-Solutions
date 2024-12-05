c4ae1b6a48107bdf7c4f163c68ab41285b466f32
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-05 13:55:44 -0600

    group anagrams

---
+++ problems/solved/medium/group_anagrams.py
@@ -0,0 +1,14 @@
+def groupAnagrams(strs):
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+
+    anagram_map = {}
+    s_dict = {}
+
+    
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(groupAnagrams([""]))
+print(groupAnagrams(["a"]))
