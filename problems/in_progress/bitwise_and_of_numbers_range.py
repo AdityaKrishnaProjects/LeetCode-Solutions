c3c15e8b2e2c5b46588298e2c4c1c6fc2432400f
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-09 13:00:51 -0600

    more work on bitwise and

--- problems/in_progress/bitwise_and_of_numbers_range.py
+++ problems/in_progress/bitwise_and_of_numbers_range.py
@@ -5,47 +5,33 @@ def rangeBitwiseAnd(left, right):
     :rtype: int
     """
     
-    l_blen = 0
-    r_blen = 0
-    int_left = left
+    blen = 0
     int_right = right
+    int_left = left
 
-    while int_left != 0:
-        if ((int_left >> 1) != 0):
-            l_blen += 1
-
-        int_left >>= 1
-
+    # Count the number of bits for which left and right are non-zero
     while int_right != 0:
-        if ((int_right >> 1) != 0):
-            r_blen += 1
+        if ((int_right >> 1) != 0) and ((int_left >> 1) == 0):
+            return 0  # If right has higher bits set than left, return 0
+        else:
+            blen += 1
+            int_right >>= 1  # Right shift to check the next bit
+            int_left >>= 1 
 
-        int_right >>= 1
+    # can't figure out how to determine what ints are good and candidates
+    poss_and = (1 << blen) - 1
+    new_left = left
+    mult = 0
 
-    if l_blen != r_blen:
-        return 0
+    while poss_and > left:
+        poss_and -= (1 << mult)
+        if poss_and > left and poss_and < right:
+            new_left &= poss_and
+        mult += 1
 
-    mult = 2**l_blen
-
-    r_int = 0
-    while (left != 0) and (right != 0):
-        if ((left-mult) > -1) and ((right-mult) > -1):
-            left -= mult
-            right -= mult
-            r_int += mult
-        elif ((right-mult) > -1):
-            right -= mult
-        elif ((left-mult) > -1):
-            left -= mult
-        mult //= 2
-
-    return r_int
+    return new_left & right
 
 print(rangeBitwiseAnd(5,7))
 print(rangeBitwiseAnd(7,7))
-print(rangeBitwiseAnd(8,16))
-print(rangeBitwiseAnd(0,0))
-print(rangeBitwiseAnd(1,2147483647))
-print(rangeBitwiseAnd(2147483646,2147483647))
-print(rangeBitwiseAnd(44,55))
-print(rangeBitwiseAnd(33,44))
+print(rangeBitwiseAnd(5,5))
+print(rangeBitwiseAnd(9,11))
