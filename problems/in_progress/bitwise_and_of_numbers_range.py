c327a67674b024039da1a64f87a41fdf472ce63b
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-09 11:36:59 -0600

    more thoughts on bitwise and of range

--- problems/in_progress/bitwise_and_of_numbers_range.py
+++ problems/in_progress/bitwise_and_of_numbers_range.py
@@ -5,18 +5,41 @@ def rangeBitwiseAnd(left, right):
     :rtype: int
     """
     
-    b_len = 0
+    l_blen = 0
+    r_blen = 0
     int_left = left
+    int_right = right
 
     while int_left != 0:
         if ((int_left >> 1) != 0):
-            b_len += 1
+            l_blen += 1
 
         int_left >>= 1
 
-    mult = 2**b_len
+    while int_right != 0:
+        if ((int_right >> 1) != 0):
+            r_blen += 1
 
-    print(mult)
+        int_right >>= 1
+
+    if l_blen != r_blen:
+        return 0
+
+    mult = 2**l_blen
+
+    r_int = 0
+    while (left != 0) and (right != 0):
+        if ((left-mult) > -1) and ((right-mult) > -1):
+            left -= mult
+            right -= mult
+            r_int += mult
+        elif ((right-mult) > -1):
+            right -= mult
+        elif ((left-mult) > -1):
+            left -= mult
+        mult //= 2
+
+    return r_int
 
 print(rangeBitwiseAnd(5,7))
 print(rangeBitwiseAnd(7,7))
@@ -24,3 +47,5 @@ print(rangeBitwiseAnd(8,16))
 print(rangeBitwiseAnd(0,0))
 print(rangeBitwiseAnd(1,2147483647))
 print(rangeBitwiseAnd(2147483646,2147483647))
+print(rangeBitwiseAnd(44,55))
+print(rangeBitwiseAnd(33,44))
