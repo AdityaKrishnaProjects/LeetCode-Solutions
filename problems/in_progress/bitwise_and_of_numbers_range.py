2bd6a0c2ded68bdc65ef5c14c2de30711f666e7d
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-10 12:47:02 -0600

    and bit manip

--- problems/in_progress/bitwise_and_of_numbers_range.py
+++ problems/in_progress/bitwise_and_of_numbers_range.py
@@ -1,29 +1,22 @@
+# extremely happy with this solution
 def rangeBitwiseAnd(left, right):
     """
     :type left: int
     :type right: int
     :rtype: int
     """
-    
-    if left == right:
-        return left
-
-    blen = 0
-    int_right = right
-    int_left = left
-
-    while int_right != 0:
-        if ((int_right >> 1) != 0) and ((int_left >> 1) == 0):
-            return 0  
-        blen += 1
-        int_right >>= 1
-        int_left >>= 1 
 
     # subtract by successive bits and then and them (1, 2, 4, 8, 16, etc.)
-    right_c = right
+    n = 1
+    while left < right:
+        right &= (right ^ n)
+        n <<= 1
+
+    return left & right
 
 print(rangeBitwiseAnd(5,7))
 print(rangeBitwiseAnd(7,7))
 print(rangeBitwiseAnd(5,5))
 print(rangeBitwiseAnd(9,11))
 print(rangeBitwiseAnd(8,9))
+print(rangeBitwiseAnd(1,283983918))
