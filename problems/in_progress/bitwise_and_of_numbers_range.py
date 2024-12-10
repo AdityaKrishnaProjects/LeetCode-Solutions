d36488ba864bec93a3b452265f0a6e4a80f9309a
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-09 22:40:40 -0600

    bits

--- problems/in_progress/bitwise_and_of_numbers_range.py
+++ problems/in_progress/bitwise_and_of_numbers_range.py
@@ -5,33 +5,25 @@ def rangeBitwiseAnd(left, right):
     :rtype: int
     """
     
+    if left == right:
+        return left
+
     blen = 0
     int_right = right
     int_left = left
 
-    # Count the number of bits for which left and right are non-zero
     while int_right != 0:
         if ((int_right >> 1) != 0) and ((int_left >> 1) == 0):
-            return 0  # If right has higher bits set than left, return 0
-        else:
-            blen += 1
-            int_right >>= 1  # Right shift to check the next bit
-            int_left >>= 1 
+            return 0  
+        blen += 1
+        int_right >>= 1
+        int_left >>= 1 
 
-    # can't figure out how to determine what ints are good and candidates
-    poss_and = (1 << blen) - 1
-    new_left = left
-    mult = 0
-
-    while poss_and > left:
-        poss_and -= (1 << mult)
-        if poss_and > left and poss_and < right:
-            new_left &= poss_and
-        mult += 1
-
-    return new_left & right
+    # subtract by successive bits and then and them (1, 2, 4, 8, 16, etc.)
+    right_c = right
 
 print(rangeBitwiseAnd(5,7))
 print(rangeBitwiseAnd(7,7))
 print(rangeBitwiseAnd(5,5))
 print(rangeBitwiseAnd(9,11))
+print(rangeBitwiseAnd(8,9))
