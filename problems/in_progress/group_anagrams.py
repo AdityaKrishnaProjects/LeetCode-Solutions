fad0710cf8ecc0a714b56d9a999be3806e60847e
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-18 11:29:44 -0600

    group anagrams using sort method

--- problems/in_progress/group_anagrams.py
+++ problems/in_progress/group_anagrams.py
@@ -1,14 +1,40 @@
+import collections
+
 def groupAnagrams(strs):
     """
     :type strs: List[str]
     :rtype: List[List[str]]
     """
 
-    anagram_map = {}
-    s_dict = {}
+    c = collections.defaultdict(list)
+    for s in strs:
+        c["".join(sorted(s))].append(s)
+    return c.values()
 
-    
+    ## Code below doesn't work because it doesn't account for duplicates. 
+    ## Should use a counter like solution
+    # anagram_dict = {}
+    # N = len(strs)
+
+    # for i in range(N):
+    #     froz = frozenset(strs[i])
+    #     if froz not in anagram_dict:
+    #         anagram_dict[froz] = [i]
+    #     else:
+    #         anagram_dict[froz].append(i)
+
+    # print(anagram_dict)
+
+    # r_list = []
+    # for ranges in anagram_dict.values():
+    #     temp_list = []
+    #     for i in ranges:
+    #         temp_list.append(strs[i])
+    #     r_list.append(temp_list)
+
+    # return r_list
 
 print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
 print(groupAnagrams([""]))
 print(groupAnagrams(["a"]))
+print(groupAnagrams(["ddddddddddg","dgggggggggg"]))
