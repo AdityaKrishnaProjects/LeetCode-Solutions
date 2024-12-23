cbe4a6fe0e60ebde92897eca6594986c8dc0a12e
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-22 21:05:49 -0600

    failing at climbing stairs... sad world we live in

--- problems/solved/easy/climbing_stairs.py
+++ problems/solved/easy/climbing_stairs.py
@@ -1,7 +1,14 @@
-def climbStairs(self, n):
+def climbStairs(n):
     """
     :type n: int
     :rtype: int
     """
     
-    
+    if n == 1:
+        return 1
+    if n == 2: 
+        return 2
+
+    return climbStairs(n-1) + climbStairs(n-2)
+
+print(climbStairs(4))
