ff304d9798978ceb2fc46cb86ffaa76c7e0b7426
AdityaKrishnaProjects <akrish2029@gmail.com>
2024-12-27 15:40:17 -0600

    memoization

--- problems/solved/easy/climbing_stairs.py
+++ problems/solved/easy/climbing_stairs.py
@@ -1,14 +1,23 @@
+# babys first memoization
 def climbStairs(n):
     """
     :type n: int
     :rtype: int
     """
     
-    if n == 1:
-        return 1
-    if n == 2: 
-        return 2
+    memo = [0] * (n+1)
 
-    return climbStairs(n-1) + climbStairs(n-2)
+    def countSteps(n):
+        if n == 0:
+            return 1
+        if n == 1: 
+            return 1
+        if memo[n]:
+            return memo[n]
+
+        memo[n] = countSteps(n-1) + countSteps(n-2)
+        return memo[n]
+
+    return countSteps(n)
 
 print(climbStairs(4))
