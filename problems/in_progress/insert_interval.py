a98419b57b7f9a75e8912065288654e30ddb3240
AdityaKrishnaProjects <akrish2029@gmail.com>
2025-05-07 15:01:53 -0500

    interval problems

---
+++ problems/in_progress/insert_interval.py
@@ -0,0 +1,35 @@
+# not clean but the logic makes sense. Find left edge, once you do, append to
+# results list and search for right edge, once you do merge remaining. 
+def insert(intervals, newInterval):
+    
+    r_list = []
+    left_edge = [newInterval[0], False]
+    right_edge = [newInterval[1], False]
+
+    for i in intervals:
+        if left_edge[0] <= i[1] and not left_edge[1]:
+            left_edge[0] = min(i[0], left_edge[0])
+            r_list.append([left_edge[0], right_edge[0]])
+            left_edge[1] = True
+        if left_edge[1] and not right_edge[1]:
+            if right_edge[0] >= i[0]:
+                right_edge[0] = max(i[1], right_edge[0])
+                r_list[-1][1] = right_edge[0]
+                continue
+            else:
+                right_edge[1] = True
+                r_list.append(i)
+                continue
+        r_list.append(i)
+
+    if not left_edge[1]:
+        r_list.append(newInterval)
+
+    return r_list
+            
+print(insert([[1,3],[6,9]], [2,5]))
+print(insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
+print(insert([[1,5]], [2,3]))
+print(insert([], [5,7]))
+print(insert([[1,5]], [6,8]))
+print(insert([[2,5],[6,7],[8,9]], [0,1]))
