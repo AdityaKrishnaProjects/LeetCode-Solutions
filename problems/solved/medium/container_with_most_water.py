ceeb97a182503ee16cbc284aeb908cf2e9692f84
AdityaKrishnaProjects <akrish2029@gmail.com>
2025-05-14 15:33:03 -0500

    two pointers

--- problems/solved/medium/container_with_most_water.py
+++ problems/solved/medium/container_with_most_water.py
@@ -1,3 +1,12 @@
+# two pointer solution works because moving the lower height value will never 
+# sacrifice a potentially larger max area (width decreases, and so you would 
+# rather move the heigher height so you can preserve a potentially larger area). 
+# In other words once you have evaluated an area for two height elements, the 
+# maximum area for those two elements has been achieved, unless there is a 
+# higher height element that will enable of the the elements to use more of its 
+# height (area is evaluated based on the smaller of the two heights). This means 
+# that you should keep the heigher value and search for this higher area, as 
+# moving the smaller value is the only way you may increase area by decreasing width.
 def maxArea(height):
 
     N = len(height)
