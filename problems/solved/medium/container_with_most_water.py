a82e089db8aacf76dcaa88580bd6d4ff6be781ca
AdityaKrishnaProjects <akrish2029@gmail.com>
2025-05-14 15:53:08 -0500

    cleaning up code

--- problems/solved/medium/container_with_most_water.py
+++ problems/solved/medium/container_with_most_water.py
@@ -20,11 +20,8 @@ def maxArea(height):
         if height[l] < height[r]:
             l += 1
 
-        elif height[l] > height[r]:
-            r -= 1
-
         else:
-            l += 1
+            r -= 1
         
     return max_area
 
