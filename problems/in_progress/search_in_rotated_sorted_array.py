58976b8f3d1d5f7b2238e6edc9ec1b8de4b3c620
AdityaKrishnaProjects <akrish2029@gmail.com>
2025-07-04 18:08:36 -0500

    progress towards bs

--- problems/in_progress/search_in_rotated_sorted_array.py
+++ problems/in_progress/search_in_rotated_sorted_array.py
@@ -1,10 +1,37 @@
-# The pivot is between index 1 and N-1, so we know that pivot is 
-# gauranteed to exist within the array. The value of our pivot 
-# is equal to the number of values that will be displaced to the 
-# end of the array in their original sorted order.
-# Searching for the pivot requires us to find the place where the 
-# array stops being strictly increasing (we know that all values 
-# are unique). We can select the middle of the array and compare 
-# out current value with our next value, we first check if this 
-# value is the pivot (if the next value is less than the current value).
+# The pivot, k is between index 1 and N-1, so we know that pivot is 
+# gauranteed to exist within the array. N-k is equal to the number
+# of values displaced to the end of the list. Any value to the right
+# of the pivot is strictly less than any value to the left of the 
+# pivot, so we know that any value less than the first value of the 
+# list must be a part of the right of the pivot (as the first value 
+# must always be to the left of the pivot). If the current value we 
+# are checking is greater than the next value it is the pivot. If 
+# not: if it is greater than the first value of the list the pivot 
+# is to the right, if it is less than the first value of the list, 
+# the pivot is to the left
 def search(nums, target):
+    
+    N = len(nums)
+
+    l, r = 0, N-1
+
+    while l < r:
+        mid = (l + r)//2
+
+        if nums[mid] > nums[mid+1]:
+            pivot = mid
+            break
+
+        if nums[mid] > nums[0]:
+            l = mid
+        else:
+            r = mid
+    
+    return pivot
+
+print(search(,))
+print(search(,))
+print(search(,))
+print(search(,))
+print(search(,))
+print(search(,))
