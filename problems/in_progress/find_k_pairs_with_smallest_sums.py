950e27c4ad43c32c1ab7e6e023130c4c6d5f8e68
AdityaKrishnaProjects <akrish2029@gmail.com>
2025-07-17 10:34:32 -0500

    heap problem progress

---
+++ problems/in_progress/find_k_pairs_with_smallest_sums.py
@@ -0,0 +1,16 @@
+def kSmallestPairs(nums1, nums2, k):
+    
+    N = len(nums1)
+    M = len(nums2)
+
+    i1, i2 = 0, 0
+
+    res = []
+
+    
+
+    return res
+
+print(kSmallestPairs([1,7,11],[2,4,6],3))
+print(kSmallestPairs([1,1,2],[1,2,3],2))
+print(kSmallestPairs([1,2,4,5,6],[3,5,7,9],3))
