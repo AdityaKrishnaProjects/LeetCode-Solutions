0212bd0b707340b78a5b92b19cb11769718a7122
AdityaKrishnaProjects <akrish2029@gmail.com>
2025-12-30 15:47:30 -0600

    edit distance progress

--- problems/in_progress/edit_distance.py
+++ problems/in_progress/edit_distance.py
@@ -9,7 +9,7 @@ string, so preserving as many characters as we can in their original position
 is the cheapest option, costing 0 operations. 
 
 Given that this is the case the first thing we must do is iterate through word1 
-and find the substring that contains the maximum number of chars in the correct 
+and find the longest substring that contains the maximum number of chars in the correct 
 order. In essence, we'd like to find the substring with the lowest cost of 
 completion in terms of replacement and insertion. Consider the following example:
 
@@ -27,9 +27,11 @@ word2: "rosti"
 word1:"omroptilrost" 8 middle, 9 end
 
 word2: "ros"
-word1: "mosrllrons"
+word1: "mosrllrs"
 
-
+Insertion has an effective cost of +1. Replacement does not (when we insert to 
+the end or middle, that means there is an extra char that will have to be 
+deleted eventually, replacing means that that char does not need to be deleted).
 
 
 
